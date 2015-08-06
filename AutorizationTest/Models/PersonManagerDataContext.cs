95e325d6f27bb90b0bab54a93abce896e85c2d7d
albert1974 <albert4dev@gmail.com>
2015-08-06 09:25:12 +0300

    v1

---
+++ AutorizationTest/Models/PersonManagerDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AutorizationTest.Models
+{
+    public class PersonManagerDataContext : DbContext
+    {
+        public PersonManagerDataContext()
+            : base("name=SqlConnection")
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
